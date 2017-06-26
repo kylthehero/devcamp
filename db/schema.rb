3e47130013230b88de3ba149906fde71351b2820
kylthehero <kylthehero@gmail.com>
2017-06-26 14:56:39 +0000

    completed authentication with virtual attrs

--- db/schema.rb
+++ db/schema.rb
@@ -49,10 +49,19 @@ ActiveRecord::Schema.define(version: 20170621143924) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "slug"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string    "email",                     default: "", null: false
+    t.string    "encrypted_password",        default: "", null: false
+    t.string    "name"
+    t.string    "reset_password_token"
+    t.datetime  "reset_password_sent_at"
+    t.datetime  "remember_created_at"
+    t.integer   "sign_in_count",            default: 0, null: false
+    t.datetime  "current_sign_in_at"
+    t.datetime  "last_sign_in_at"
+    t.inet      "current_sign_in_ip"
+    t.inet      "last_sign_in_ip"
+    t.datetime  "created_at",               null: false
+    t.datetime  "updated_at",               null: false
     t.index ["slug"], name: "index_users_on_slug", unique: true
   end
 
